7c80943000f51f9e2df9541a11cf9c87efaa023f
Corbin Ball <c.ball@me.com>
2021-10-08 11:24:46 -0500

    added questions for user input

--- index.js
+++ index.js
@@ -4,7 +4,60 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: "projectTitle",
+        message: "What is your project titled?",
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Pretty please give a description of your project."
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "How would a user install this sweet project?",
+    },
+    {
+        type: "input",
+        name: "useageInfo",
+        message: "What is this sweet project used for?",
+    },
+    {
+        type: "input",
+        name: "contributions",
+        message: "Who all contributed to this project?",
+    },
+    {
+        type: "input",
+        name: "testInst",
+        message: "What are the instructions for a test?",
+    },
+    {
+        type: "list",
+        name: "licenseChoice",
+        message: "Please choose the license for this project:",
+        choices: [
+            "Apache License 2.0",
+            "GNU General Public License v3.0",
+            "MIT License",
+            "Mozilla Public License 2.0",
+            "The Unlicense",
+        ]
+    },
+    {
+        type: "input",
+        name: "githubName",
+        message: "What is your GitHub username?",
+    },
+    {
+        type: "input",
+        name: "yourEmail",
+        message: "What is your email for project Questions?"
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
