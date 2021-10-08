9207425f1c796791d61f1f25bf21e43f01c967f0
Corbin Ball <c.ball@me.com>
2021-10-08 14:39:16 -0500

    working on license rendering function

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,22 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  let licenseType = `${answers.licenseChoice}`;
+  let theLicense = '';
+  if(licenseType === 'MIT License') {
+    theLicense = `![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`
+  } else if (licenseType === 'GNU General Public License v3.0') {
+    theLicense = `![License: GPL v3](https://img.shields.io/badge/License-GPL%20v3-blue.svg)](http://www.gnu.org/licenses/gpl-3.0)`
+  } else if (licenseType === 'Apache License 2.0') {
+    theLicense = `![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`
+  } else if (licenseType === 'Mozilla Public License 2.0') {
+    theLicense = `![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)`
+  } else {
+    licenseType = "N/A"
+  };
+  return theLicense;
+};
+
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -43,8 +59,7 @@ function generateMarkdown(answers) {
   ${answers.contributions}
   <br>
   ## License
-  ${answers.licenseChoice}
-  // use functions for license badge/info
+  ${renderLicenseBadge(license)}
 `;
 }
 
